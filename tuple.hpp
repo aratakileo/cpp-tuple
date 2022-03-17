d79c4e98d3ed911bdedda4ca21b96cbdd37dd833
Tea Condemns <83653555+TeaCondemns@users.noreply.github.com>
2022-03-17 12:49:42 +0300

    Update tuple.hpp

--- tuple.hpp
+++ tuple.hpp
@@ -4,37 +4,37 @@
 #include <iostream>
 
 namespace tuple {
-	// item of tuple
-	template<std::size_t i, typename item>
-	struct leaf {
-	    item value;
-	};
-	
-	
-	template<std::size_t i, typename... items>
-	struct impl;
-	
-	
-	// empty tuple
-	template<std::size_t i>
-	struct impl<i>{};
-	
-	
-	template<std::size_t i, typename item, typename... items>
-	struct impl<i, item, items...> :
-	    public leaf<i, item>,
-	    public impl<i + 1, items...> {};
-	
+    // item of tuple
+    template<std::size_t i, typename item>
+    struct leaf {
+        item value;
+    };
 
-	// at<0>(tuple) = ...;
-	// ... = at<0>(tuple);
-	template<std::size_t i, typename item, typename... items>
-	item& at(impl<i, item, items...>& _tuple) {
-	    return _tuple.leaf<i, item>::value;
-	}
-	
-	
-	template<typename... items>
-	using tuple = impl<0, items...>;
+
+    template<std::size_t i, typename... items>
+    struct impl;
+
+
+    // empty tuple
+    template<std::size_t i>
+    struct impl<i>{};
+
+
+    template<std::size_t i, typename item, typename... items>
+    struct impl<i, item, items...> :
+        public leaf<i, item>,
+        public impl<i + 1, items...> {};
+
+
+    // at<0>(tuple) = ...;
+    // ... = at<0>(tuple);
+    template<std::size_t i, typename item, typename... items>
+    item& at(impl<i, item, items...>& _tuple) {
+        return _tuple.leaf<i, item>::value;
+    }
+
+
+    template<typename... items>
+    using tuple = impl<0, items...>;
 }
 #endif
