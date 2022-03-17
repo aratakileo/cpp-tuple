a8a4cd4bd9001fa8755983b4fa01649b67c3443f
Tea Condemns <83653555+TeaCondemns@users.noreply.github.com>
2022-03-17 12:46:51 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "tuple.hpp"
+#include <iostream>
+#include <string>
+
+int main() {
+    tuple::tuple<int, float, std::string> _tuple;
+
+    tuple::at<0>(_tuple) = 123;
+    tuple::at<1>(_tuple) = 12.3;
+    tuple::at<2>(_tuple) = "Hello World!";
+
+    std::cout << tuple::at<0>(_tuple) << std::endl;
+    std::cout << tuple::at<1>(_tuple) << std::endl;
+    std::cout << tuple::at<2>(_tuple) << std::endl;
+
+    return 0;
+}
